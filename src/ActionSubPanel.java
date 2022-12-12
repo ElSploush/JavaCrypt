d859430898fccdfbe20fe062045fa4b45b3b675f
ElSploush <119965349+ElSploush@users.noreply.github.com>
2022-12-12 09:09:20 -0800

    Commit v1.5
    Changed icon names

--- src/ActionSubPanel.java
+++ src/ActionSubPanel.java
@@ -29,8 +29,8 @@ public class ActionSubPanel extends JPanel
 		buttonArray[0] = new AttackButton("Attack"); // Set title of button
 		buttonArray[0].setVerticalTextPosition(SwingConstants.BOTTOM); // Set position of title vertically 
 		buttonArray[0].setHorizontalTextPosition(SwingConstants.CENTER); // Set position of title horizontally
-		buttonArray[0].setIcon(new ImageIcon("attack.png")); // Set image icon
-		buttonArray[0].setRolloverIcon(new ImageIcon("attack.gif")); // Set rollover icon
+		buttonArray[0].setIcon(new ImageIcon("attackicon.png")); // Set image icon
+		buttonArray[0].setRolloverIcon(new ImageIcon("attackicon.gif")); // Set rollover icon
 		buttonArray[0].setRolloverEnabled(true); // Enable icon
 		AttackButtonListener attackButtonListener = new AttackButtonListener((AttackButton) buttonArray[0], game);
 		buttonArray[0].addActionListener(attackButtonListener);
@@ -39,8 +39,8 @@ public class ActionSubPanel extends JPanel
 		buttonArray[1] = new HealButton("Heal"); 
 		buttonArray[1].setVerticalTextPosition(SwingConstants.BOTTOM);
 		buttonArray[1].setHorizontalTextPosition(SwingConstants.CENTER);
-		buttonArray[1].setIcon(new ImageIcon("heal.png"));
-		buttonArray[1].setRolloverIcon(new ImageIcon("heal.gif")); // Set rollover icon
+		buttonArray[1].setIcon(new ImageIcon("healicon.png"));
+		buttonArray[1].setRolloverIcon(new ImageIcon("healicon.gif")); // Set rollover icon
 		buttonArray[1].setRolloverEnabled(true); // Enable icon
 		HealButtonListener healButtonListener = new HealButtonListener((HealButton) buttonArray[1], game);
 		buttonArray[1].addActionListener(healButtonListener);
@@ -49,8 +49,8 @@ public class ActionSubPanel extends JPanel
 		buttonArray[2] = new FleeButton("Flee"); 
 		buttonArray[2].setVerticalTextPosition(SwingConstants.BOTTOM);
 		buttonArray[2].setHorizontalTextPosition(SwingConstants.CENTER);
-		buttonArray[2].setIcon(new ImageIcon("flee.png"));
-		buttonArray[2].setRolloverIcon(new ImageIcon("flee.gif")); // Set rollover icon
+		buttonArray[2].setIcon(new ImageIcon("fleeicon.png"));
+		buttonArray[2].setRolloverIcon(new ImageIcon("fleeicon.gif")); // Set rollover icon
 		buttonArray[2].setRolloverEnabled(true); // Enable icon
 		FleeButtonListener fleeButtonListener = new FleeButtonListener((FleeButton) buttonArray[2], game);
 		buttonArray[2].addActionListener(fleeButtonListener);
